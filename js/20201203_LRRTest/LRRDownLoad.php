82570491707ac7228216413eac3f8b51a8b2d45c
shiro <jobhiromobile-iroiro@yahoo.co.jp>
2020-12-03 19:31:27 +0900

    20201203 やり直し

--- js/20201203_LRRTest/LRRDownLoad.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $JSON = file_get_contents('php://input');
-    $date = json_decode($JSON, false);
-    $readFileName = "LRRSaveData/LRRSaveData_".$date->year."-".$date->month."-".$date->date.".json";
-    //echo $readFileName;
-    readfile($readFileName);
-?>
--- js/20201203_LRRTest/LRRUpLoad.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $JSON = file_get_contents('php://input');
-    $date = json_decode($JSON, false)->calender;
-    $outputFileName = "LRRSaveData/LRRSaveData_".$date->year."-".$date->month."-".$date->date.".json";
-    file_put_contents($outputFileName, $JSON);
-?>
